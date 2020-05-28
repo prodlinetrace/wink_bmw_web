10721b3571a3ac2e42990d36006ccf6a7c054e7b
Piotr Wilkosz <piotr.wilkosz@gmail.com>
2020-05-29 01:19:29 +0200

    change development port to 5500

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ from app import create_app
 app = create_app('default')
 
 if __name__ == "__main__":
-    app.run(debug=True, host='127.0.0.1', threaded=True)
+    app.run(debug=True, host='127.0.0.1',  port=5500, threaded=True)
